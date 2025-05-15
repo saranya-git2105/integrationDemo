588323743f33af42bbac8d996f04acc230ca17a0
unknown <karun@idnor.com>
2025-05-15 16:00:21 +0530

    changed Z index for the tooltip

--- react-widget/src/components/CustomSmoothEdge.jsx
+++ react-widget/src/components/CustomSmoothEdge.jsx
@@ -52,7 +52,6 @@ const CustomSmoothEdge = ({
             fontSize: 12,
             pointerEvents: 'none',
             boxShadow: '0 1px 3px rgba(0,0,0,0.1)',
-            zIndex: 1000,
           }}
         >
           {label}
